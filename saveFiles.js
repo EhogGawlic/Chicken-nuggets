0dd826a47e3906abda5f14e5f94da957667bfd1a
ehogin <ethang0227@gmail.com>
2026-08-21 15:13:50 +0000

    added fibonacci sequence code

---
+++ saveFiles.js
@@ -0,0 +1,14 @@
+function generateSaveFile(gameState){
+    return JSON.stringify(gameState);
+}
+function downloadSaveFile(saveData, filename){
+    const blob = new Blob([saveData], { type: 'application/json' });
+    const url = URL.createObjectURL(blob);
+    const a = document.createElement('a');
+    a.href = url;
+    a.download = filename;
+    document.body.appendChild(a);
+    a.click();
+    document.body.removeChild(a);
+    URL.revokeObjectURL(url);
+}
